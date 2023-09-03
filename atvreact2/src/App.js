afbed1020ed81355cfd88036b7aab016e064cc5f
PetersonBarbos <128330730+PetersonBarbos@users.noreply.github.com>
2023-09-03 18:40:19 -0300

    .

---
+++ atvreact2/src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import CloneFace from './Componentes/CloneFace';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CloneFace/>
+    </div>
+  );
+}
+
+export default App;
---
+++ atvreact2/src/Componentes/CloneFace.js
@@ -0,0 +1,27 @@
+function CloneFace (){
+    return(
+        <div>
+            <span className = "lateral">
+                <p>facebook</p>
+                <p>O Facebook ajuda você a se conectar e compartilhar com as pessoas que fazem parte da sua vida.</p>
+                </span>
+                <span className = "lateral">
+                    <form>
+                        <p><input type = "email" autocomplete = "on" placeholder ="email ou telefone"></input></p>
+                        <p><input type ="password" placeholder = "senha"></input></p>
+                        <a href="https://pt-br.facebook.com/" target="_blank" rel="noreferrer">
+                            <p><input type = "button" value = "Entrar"></input></p>
+                        </a>
+                    </form>
+                    <a href="https://pt-br.facebook.com/" target="_blank" rel="noreferrer">
+                        <p>esqueceu a senha?</p>
+                    </a>
+                    <hr></hr>
+                    <a href="https://pt-br.facebook.com/" target="_blank" rel="noreferrer">
+                        <input type = "button" value = "Criar nova conta"></input>
+                        </a>
+            </span>
+            <p><a href="https://pt-br.facebook.com/" target="_blank" rel="noreferrer"><strong>Criar uma Página para uma celebridade</strong></a>, uma marca ou uma empresa.</p>
+        </div>
+    )
+} export default CloneFace;
