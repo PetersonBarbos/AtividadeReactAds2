6b1178f3e771785ada2f77edb92a90fd002ca9b3
PetersonBarbos <128330730+PetersonBarbos@users.noreply.github.com>
2023-09-10 17:18:08 -0300

    .

--- atvreact2/src/Componentes/CloneFace.js
+++ atvreact2/src/Componentes/CloneFace.js
@@ -1,29 +1,25 @@
 function CloneFace (){
     return(
-        <div>
-            <span className = "lateral" id ="naodestacado">
-                <p>facebook</p>
+        <div className = "lateral">
+            <div id ="naodestacado">
+                <p id = "maior">facebook</p>
                 <p>O Facebook ajuda você a se conectar e compartilhar com as pessoas que fazem parte da sua vida.</p>
-                </span>
-                <span className = "lateral">
-                    <span id="destacado"> 
+            </div>
+            <div>
+                <div id="destacado">
                         <form>
-                            <p><input type = "email" autocomplete = "on" placeholder ="email ou telefone"></input></p>
-                            <p><input type ="password" placeholder = "senha"></input></p>
-                            <a href="https://pt-br.facebook.com/" target="_blank" rel="noreferrer">
-                                <p><input type = "button" value = "Entrar"></input></p>
-                            </a>
+                            <p><input type = "email" autocomplete = "on" placeholder ="email ou telefone" className="caixas"></input></p>
+                            <p><input type ="password" placeholder = "senha" className="caixas"></input></p>
+                            <p><input type = "button" value = "Entrar" id="entrar"></input></p>
                         </form>
-                        <a href="https://pt-br.facebook.com/" target="_blank" rel="noreferrer">
                             <p>esqueceu a senha?</p>
-                        </a>
                         <hr></hr>
-                        <a href="https://pt-br.facebook.com/" target="_blank" rel="noreferrer">
-                            <input type = "button" value = "Criar nova conta"></input>
-                        </a>
-                    </span>
-                    <p><a href="https://pt-br.facebook.com/" target="_blank" rel="noreferrer"><strong>Criar uma Página para uma celebridade</strong></a>, uma marca ou uma empresa.</p>
-            </span>
+                            <input type = "button" value = "Criar nova conta" id="verde"></input>
+                </div>
+                    <div id="abaixo">
+                        <p><strong>Criar uma Página para uma celebridade</strong>, uma marca ou uma empresa.</p>
+                    </div>
+                </div>
         </div>
     )
 } export default CloneFace;
